6566313d98e4788a85972637a1cb18a3e64148a0
Thomas Lindae <thomas.lindae@in.tum.de>
2021-02-21 09:19:24 +0100

    encoding works correctly now

--- src/encode.rs
+++ src/encode.rs
@@ -1,10 +1,9 @@
 use crate::tree_util::HufTreeNode;
 use ahash::AHashMap;
 use bitvec::prelude::*;
-use bitvec::slice as bv_slice;
 use std::collections::VecDeque;
 
-pub fn encode(input_data: &[u8]) -> (Vec<HufTreeNode>, Vec<u8>) {
+pub fn encode(input_data: &[u8]) -> (Vec<HufTreeNode>, BitVec) {
     // frequency analysis
     let mut frequency_map: AHashMap<u8, usize> = AHashMap::new();
 
@@ -17,21 +16,21 @@ pub fn encode(input_data: &[u8]) -> (Vec<HufTreeNode>, Vec<u8>) {
     let tree_size = (frequency_map.len() * 2) - 1;
     let mut huffman_tree: Vec<HufTreeNode> = Vec::with_capacity(tree_size);
 
-    for (val, freq) in frequency_map {
+    for (&val, &freq) in frequency_map.iter() {
         huffman_tree.push(HufTreeNode::new(val, freq, -1, -1));
     }
 
     huffman_tree.sort_unstable_by_key(|k| k.freq);
 
-    let mut queue1: VecDeque<i16> = (0..(tree_size as i16)).collect();
-    let mut queue2: VecDeque<i16> = VecDeque::with_capacity(tree_size);
+    let mut queue1: VecDeque<usize> = (0..frequency_map.len()).collect();
+    let mut queue2: VecDeque<usize> = VecDeque::with_capacity(frequency_map.len());
 
-    let mut j = 0;
+    let mut j = huffman_tree.len();
     while (queue1.len() + queue2.len()) > 1 {
         let (elem1, freq1) = take_less(&mut queue1, &mut queue2, &huffman_tree);
         let (elem2, freq2) = take_less(&mut queue1, &mut queue2, &huffman_tree);
 
-        huffman_tree.push(HufTreeNode::new(0, freq1 + freq2, elem1, elem2));
+        huffman_tree.push(HufTreeNode::new(0, freq1 + freq2, elem1 as i16, elem2 as i16));
 
         queue2.push_back(j);
         j += 1;
@@ -39,7 +38,7 @@ pub fn encode(input_data: &[u8]) -> (Vec<HufTreeNode>, Vec<u8>) {
 
     // create lookup table for codewords
     let mut codewords: BitVec = BitVec::new();
-    let mut lookup_table: AHashMap<u8, &BitSlice> = AHashMap::new();
+    let mut lookup_table: AHashMap<u8, (usize, usize)> = AHashMap::new();
 
     traverse_tree(
         huffman_tree.len() - 1,
@@ -50,46 +49,53 @@ pub fn encode(input_data: &[u8]) -> (Vec<HufTreeNode>, Vec<u8>) {
     );
 
     // encoding original data
+    let mut bitsequence: BitVec = BitVec::new();
 
-    (Vec::new(), Vec::new())
+    for c in input_data {
+        let indices = lookup_table.get(c).unwrap();
+        let slice = &codewords[indices.0..indices.1 + 1];
+
+        bitsequence.extend_from_bitslice(slice);
+    }
+
+    (huffman_tree, bitsequence)
 }
 
-// currently not working
-// see https://stackoverflow.com/questions/66289524/rust-collecting-slices-of-a-vec-in-a-recursive-function
-fn traverse_tree<'a>(
+fn traverse_tree(
     cur_index: usize,
-    height: i16,
-    codewords: &'a mut bitvec::prelude::BitVec,
-    lookup_table: &mut AHashMap<u8, &'a BitSlice>,
+    height: usize,
+    codewords: &mut bitvec::prelude::BitVec,
+    lookup_table: &mut AHashMap<u8, (usize, usize)>,
     huffman_tree: &[HufTreeNode],
 ) {
     let cur_node = &huffman_tree[cur_index];
 
     // if the left child is -1, we reached a leaf
     if cur_node.left == -1 {
-        let cur_sequence = &codewords[(codewords.len() - 1 - height as usize)..];
-        lookup_table.insert(cur_node.val, cur_sequence);
+        let l_ind = codewords.len() - height;
+        let r_ind = codewords.len() - 1;
+        lookup_table.insert(cur_node.val, (l_ind, r_ind));
         return;
     }
 
     // save the current sequence so we can traverse to the right afterwards
-    let mut cur_sequence = codewords[(codewords.len() - 1 - height as usize)..].to_bitvec();
+    let mut cur_sequence = codewords[(codewords.len() - height as usize)..].to_bitvec();
     codewords.push(false);
     traverse_tree(
         cur_node.left as usize,
         height + 1,
-        codewords, // mutable borrow - argument requires that `*codewords` is borrowed for `'a`
+        codewords,
         lookup_table,
         huffman_tree,
     );
-
     // append the previously saved current sequence
-    codewords.extend(&mut cur_sequence); // second mutable borrow occurs here
-    codewords.push(true); // third mutable borrow occurs here
+    codewords.append(&mut cur_sequence);
+
+    codewords.push(true);
     traverse_tree(
         cur_node.right as usize,
         height + 1,
-        codewords, // fourth mutable borrow occurs here
+        codewords,
         lookup_table,
         huffman_tree,
     );
@@ -97,17 +103,17 @@ fn traverse_tree<'a>(
 
 #[inline]
 fn take_less(
-    queue1: &mut VecDeque<i16>,
-    queue2: &mut VecDeque<i16>,
+    queue1: &mut VecDeque<usize>,
+    queue2: &mut VecDeque<usize>,
     huffman_tree: &[HufTreeNode],
-) -> (i16, usize) {
+) -> (usize, usize) {
     match queue1.front() {
         Some(&i1) => {
-            let freq1 = huffman_tree[i1 as usize].freq;
+            let freq1 = huffman_tree[i1].freq;
 
             match queue2.front() {
                 Some(&i2) => {
-                    let freq2 = huffman_tree[i2 as usize].freq;
+                    let freq2 = huffman_tree[i2].freq;
 
                     if freq1 <= freq2 {
                         queue1.pop_front();
@@ -125,7 +131,7 @@ fn take_less(
         }
         None => {
             let i2 = queue2.pop_front().unwrap();
-            let freq2 = huffman_tree[i2 as usize].freq;
+            let freq2 = huffman_tree[i2].freq;
             (i2, freq2)
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@ mod decode;
 mod encode;
 mod tree_util;
 
+use encode::encode;
+use decode::decode;
+
 fn main() {
-    encode::encode(b"sesamstrasse");
+    let result = encode(b"sesamstrasse");
+    println!("{:?}", result.1);
 }
