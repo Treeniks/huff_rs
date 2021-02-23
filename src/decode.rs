fd53dd17268316c836b6fb0cdb8f43c037b15b0a
Thomas Lindae <thomas.lindae@in.tum.de>
2021-02-23 08:58:25 +0100

    implemented argument parsing

--- src/decode.rs
+++ src/decode.rs
@@ -3,22 +3,22 @@ use crate::tree_util::ShortHufTreeNode;
 use bitvec::prelude::*;
 
 pub fn decode(tree: &[ShortHufTreeNode], input_data: &BitSlice<Lsb0, u8>) -> Vec<u8> {
-	let mut result = Vec::new();
+    let mut result = Vec::new();
 
-	let root_node = tree[tree.len() - 1];
-	let mut cur_node = root_node;
-	for i in input_data {
-		if i == false {
-			cur_node = tree[cur_node.left as usize];
-		} else {
-			cur_node = tree[cur_node.right as usize];
-		}
+    let root_node = tree[tree.len() - 1];
+    let mut cur_node = root_node;
+    for i in input_data {
+        if i == false {
+            cur_node = tree[cur_node.left as usize];
+        } else {
+            cur_node = tree[cur_node.right as usize];
+        }
 
-		if cur_node.left == -1 {
-			result.push(cur_node.val);
-			cur_node = root_node;
-		}
-	}
+        if cur_node.left == -1 {
+            result.push(cur_node.val);
+            cur_node = root_node;
+        }
+    }
 
-	return result;
+    return result;
 }
--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,13 @@ mod tree_util;
 use decode::decode;
 use encode::encode;
 
-use std::env;
 use std::fs;
 use std::fs::OpenOptions;
 use std::io::Write;
+
+#[macro_use]
+extern crate clap;
+use clap::{App};
 
 unsafe fn any_as_u8_slice<T: Sized>(x: &T) -> &[u8] {
     std::slice::from_raw_parts((x as *const T) as *const u8, std::mem::size_of::<T>())
@@ -21,39 +24,43 @@ unsafe fn slice_as_u8_slice<T: Sized>(x: &[T]) -> &[u8] {
     )
 }
 
-use bitvec::prelude::*;
+fn main() -> Result<(), std::io::Error> {
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
+    if let Some(matches) = matches.subcommand_matches("encode") {
+        let input_filename = matches.value_of("input").unwrap();
+        let output_filename = matches.value_of("output").unwrap_or("output.huf");
 
-    // println!("{}", std::mem::size_of::<tree_util::ShortHufTreeNode>());
+        let input_data = fs::read(input_filename)?;
+        let result = encode(&input_data);
+        let mut output_file = OpenOptions::new()
+            .create(true)
+            .write(true)
+            .truncate(true)
+            .open(output_filename)
+            .unwrap();
 
-    let input_filename = &args[1];
-    let output_filename = &args[2];
+        output_file.write_all(unsafe { any_as_u8_slice(&input_data.len()) })?; // original file's length
+        output_file.write_all(unsafe { any_as_u8_slice(&(result.0.len() as u16)) })?; // size of huffman_tree
+        output_file.write_all(unsafe { slice_as_u8_slice(&result.0) })?; // huffman_tree
+        output_file.write_all(&[result.2])?; // fillup
+        output_file.write_all(result.1.as_raw_slice())?; // bitsequence
+    } else if let Some(matches) = matches.subcommand_matches("decode") {
+        let input_filename = matches.value_of("input").unwrap();
+        let output_filename = matches.value_of("output").unwrap_or("output.huf");
 
-    let input_data = fs::read(input_filename).expect("Something went wrong reading the file");
+        let input_data = fs::read(input_filename)?;
+        // TODO extract tree and bitsequence and pass it to decode
+    }
 
-    let result = encode(&input_data);
-
-    let mut output_file = OpenOptions::new()
-        .create(true)
-        .write(true)
-        .truncate(true)
-        .open(output_filename)
-        .unwrap();
-
-    output_file.write_all(unsafe { any_as_u8_slice(&input_data.len()) }); // original file's length
-    output_file.write_all(unsafe { any_as_u8_slice(&(result.0.len() as u16)) }); // size of huffman_tree
-    output_file.write_all(unsafe { slice_as_u8_slice(&result.0) }); // huffman_tree
-    output_file.write_all(&[result.2]); // fillup
-    output_file.write_all(result.1.as_raw_slice()); // bitsequence
-    // println!("{:?}", result.1);
+    Ok(())
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::encode::encode;
     use crate::decode::decode;
+    use crate::encode::encode;
 
     #[test]
     fn encode_decode_same_result() {
